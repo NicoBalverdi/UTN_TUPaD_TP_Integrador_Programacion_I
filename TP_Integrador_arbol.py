cd58092e899832f752025795837147520318a1b1
NicoBalverdi <nicba96@gmail.com>
2025-06-09 18:52:41 -0300

    Comentarios a las funciones

--- TP_Integrador_arbol.py
+++ TP_Integrador_arbol.py
@@ -22,100 +22,102 @@ def crear_arbol():
     continuar = 1 # Bandera para que el usuario controle el ingreso de inputs
     while continuar == 1: # Condición del ciclo donde utilizamos la bandera
         valor = input("Ingrese un código: ") # Registro del input del usuario
-        if valor.isdigit():
-            codigo = int(valor)
-            arbol = insertar(arbol, codigo)
+        if valor.isdigit(): # Verificación de tipo de input
+            codigo = int(valor) # Casteo del string a entero
+            arbol = insertar(arbol, codigo) # Se le pasan los argumentos a la función insertar
         else:
-            print("No se permiten códigos negativos o letras, el sistema trabaja con inputs numéricos")
-        continuar = int(input("Desea ingresar otro código? (1. Si / 2. No): "))
-    return arbol
+            print("No se permiten códigos negativos o letras, el sistema trabaja con inputs numéricos") # Si el input no es un dígito, se le informa al usuario del error
+        continuar = int(input("Desea ingresar otro código? (1. Si / 2. No): ")) # Se le da la opción de continuar o terminar el ingreso de valores al usuario
+    return arbol # Se devuelve el árbol resultante (ABB)
 
+# Graficar los elementos en forma de árbol
 def mostrar_arbol(arbol, nivel=0):
-    if arbol != []:
-        mostrar_arbol(arbol[2], nivel + 1)
-        print("    " * nivel + f"→ {arbol[0]}")
-        mostrar_arbol(arbol[1], nivel + 1)
+    if arbol != []: # Verificar que el árbol (lista o lista anidada) no este vacío.
+        mostrar_arbol(arbol[2], nivel + 1) # Se llama recursivamente la función para la lista anidada con un valor mayor al nodo padre
+        print("    " * nivel + f"→ {arbol[0]}") # Si no lo esta, el valor en la pos #0 se imprime en el centro, si es una lista anidada dependiendo del nivel tendrá una cantidad de espacios entre el nodo y la raíz
+        mostrar_arbol(arbol[1], nivel + 1) # Se llama recursivamente la función para la lista anidada con un valor inferior al nodo padre
 
+# Buscar y graficar la ruta a un valor
 def encontrar_ruta(arbol, valor, ruta=[]):
-    if arbol == []:
-        return []
+    if arbol == []: # Si el arbol esta vacío
+        return [] # Se retorna una lista vacía
 
-    ruta = ruta + [arbol[0]]
+    ruta = ruta + [arbol[0]] # Se agrega el valor de la pos #0 a la lista ruta
 
-    if valor == arbol[0]:
-        return ruta
-    elif valor < arbol[0]:
-        return encontrar_ruta(arbol[1], valor, ruta)
-    else:
-        return encontrar_ruta(arbol[2], valor, ruta)
+    if valor == arbol[0]: # Si el valor es igual a la pos #0 del árbol o la lista anidada entonces llegamos a destino
+        return ruta # Retornamos la ruta
+    elif valor < arbol[0]: # Sino verificamos si el valor menor a la pos #0 del árbol o lista anidada
+        return encontrar_ruta(arbol[1], valor, ruta) # Se llama recursivamente a la función para buscar el valor en la lista anidada izquierda
+    else: # Si el valor era mayor...
+        return encontrar_ruta(arbol[2], valor, ruta) # Se llama recursivamente a la función para buscar el valor en la lista anidada derecha
 
 def mostar_ruta(ruta = list):
-    if ruta == []:
-        return []
-    else:
-        for i in range(0, len(ruta)):
-            if i == 0:
-                print(ruta[i], end="")
-            else:
-                print(f" → {ruta[i]}", end="")
+    if ruta == []: # Si ruta esta vacía
+        return [] # Se retorna una ruta vacía
+    else: # Sino...
+        for i in range(0, len(ruta)): # Se recorre una lista utilizando el ciclo for segun su longitud
+            if i == 0: # Para la posición #0...
+                print(ruta[i], end="") # Se imprime ruta pos # 0 y se indica que los siguientes prints se imprimiran al lado en lugar de realizar un salto de linea
+            else: # Si no es la posición #0...
+                print(f" → {ruta[i]}", end="") # Se imprime el próximo valor de la lista separandolo del primero con una flecha entre ambos para graficar la ruta recorrida
 
 # Modificar un nodo
 
-def minimo(arbol):
-    actual = arbol
-    while actual[1] != []:
-        actual = actual[1]
-    return actual[0]
+def minimo(arbol): # función que determinará el valor mínimo de una lista anidada en caso de modificar un nodo con hijos
+    actual = arbol # Toma como arbol actual para trabajar el subarbol donde se encuentra el nodo a modifciar y su hijo/s
+    while actual[1] != []: # Si el nodo padre tiene un hijo menor
+        actual = actual[1] # El subarbol ahora se convierte en una lista donde se encuentra únicamente el valor del hijo menor
+    return actual[0] # Se retorna el hijo menor
 
-def maximo(arbol):
-    actual = arbol
-    while actual[2] != []:
-        actual = actual[2]
-    return actual[0]
+def maximo(arbol): # función que determinará el valor máximo de una lista anida en caso de modificar un nodo con hijos
+    actual = arbol # Toma como arbol actual para trabajar el subarbol donde se encuentra el nodo a modifciar y su hijo/s
+    while actual[2] != []: # Si el nodo padre tiene un hijo mayor
+        actual = actual[2] # El subarbol ahora se convierte en una lista donde se encuentra únicamente el valor del hijo mayor
+    return actual[0] # Se retorna el hijo mayor
 
-def modificar_nodo(arbol, valor_antiguo, nuevo_valor, padre=[], es_izquierdo=bool):
-    if arbol == []:
-        return []
+def modificar_nodo(arbol, valor_antiguo, nuevo_valor, padre=[], es_izquierdo=bool): # La función principal para modificar un nodo
+    if arbol == []: # Si el árbol o subarbol esta vacío...
+        return [] # Se retorna una lista vacía
 
-    if valor_antiguo < arbol[0]:
-        modificar_nodo(arbol[1], valor_antiguo, nuevo_valor, arbol, True)
-    elif valor_antiguo > arbol[0]:
-        modificar_nodo(arbol[2], valor_antiguo, nuevo_valor, arbol, False)
+    if valor_antiguo < arbol[0]: # Por otra parte si el valor antiguo es menor al valor en la pos #0 del arbol/subarbol debemos buscar en el subarbol izquierdo
+        modificar_nodo(arbol[1], valor_antiguo, nuevo_valor, arbol, True) # Llamamos recursivamente a la función para que explore el subarbol izquierdo pero esta vez agregamos el arbol padre y le indicamos que efectivamente el valor es del lado izquierdo
+    elif valor_antiguo > arbol[0]: # Si el valor antiguo es mayor...
+        modificar_nodo(arbol[2], valor_antiguo, nuevo_valor, arbol, False) # Se buscará en el subarbol derecho, tambien enviamos el arbol padre e indicamos que no es un valor del subarbol izquierdo
     else:
         # Validación con padre
         if padre != []:
-            if es_izquierdo and nuevo_valor >= padre[0]:
-                print(f"Un código a la izqierda de {padre[0]} no puede ser mayor. Si necesita cargarlo, utilice insertar.")
-                return
-            elif not es_izquierdo and nuevo_valor <= padre[0]:
-                print(f"Un código a la derecha de {padre[0]} no puede ser menor. Si necesita cargarlo, utilice insertar.")
-                return
+            if es_izquierdo and nuevo_valor >= padre[0]: # Si es izquierdo y el nuevo valor es mayor o igual al valor del padre...
+                print(f"Un código a la izqierda de {padre[0]} no puede ser mayor. Si necesita cargarlo, utilice insertar.") # Se le indica el problema al usuario
+                return # El return termina el proceso
+            elif not es_izquierdo and nuevo_valor <= padre[0]: # Si el valor es derecho y es menor o igual al padre...
+                print(f"Un código a la derecha de {padre[0]} no puede ser menor. Si necesita cargarlo, utilice insertar.") # Se le indica el problema al usuario
+                return # Se termina el proceso
         # Validación con hijos
-        if arbol[1] != [] and nuevo_valor <= maximo(arbol[1]):
-            print(f"{nuevo_valor} no puede ser menor o igual a {maximo(arbol[1])}, utilice la operación insertar si desea cargar {nuevo_valor} en el sistema")
-            return
-        if arbol[2] != [] and nuevo_valor >= minimo(arbol[2]):
-            print(f"{nuevo_valor} no puede ser mayor o igual a {minimo(arbol[2])}, utilice la operación insertar si desea cargar {nuevo_valor} en el sistema.")
-            return
-        arbol[0] = nuevo_valor
-        print(f"Se ha modificado correctamente {valor_antiguo} por {nuevo_valor}")
-    return arbol
+        if arbol[1] != [] and nuevo_valor <= maximo(arbol[1]): # Si existe un subarbol izquierdo y el nuevo valor es menor o igual al hijo más grande de ese subarbol...
+            print(f"{nuevo_valor} no puede ser menor o igual a {maximo(arbol[1])}, utilice la operación insertar si desea cargar {nuevo_valor} en el sistema") # Se explica el error
+            return # Se termina el proceso
+        if arbol[2] != [] and nuevo_valor >= minimo(arbol[2]): # Si existe un subarbol derecho y el nuevo valor es mayor o igual al hijo más chico de ese subarbol...
+            print(f"{nuevo_valor} no puede ser mayor o igual a {minimo(arbol[2])}, utilice la operación insertar si desea cargar {nuevo_valor} en el sistema.") # Se notifica el error
+            return # Se termina el proceso
+        arbol[0] = nuevo_valor # Si se pasaron todas las verificaciones, se reemplaza el valor viejo por el nuevo
+        print(f"Se ha modificado correctamente {valor_antiguo} por {nuevo_valor}") # Se notifica al usuario de que no hubo inconvenientes y el cambio fue efecutado
+    return arbol # Se retorna el arbol
 
 # Los 3 recorridos de los arboles binarios
-def recorrer_preorden(arbol):
-    if arbol == []:
-        return []
-    return [arbol[0]] + recorrer_preorden(arbol[1]) + recorrer_preorden(arbol[2])
+def recorrer_preorden(arbol): # Se ingresa un arbol
+    if arbol == []: # Si el arbol esta vacio...
+        return [] # Se retorna una lista vacía
+    return [arbol[0]] + recorrer_preorden(arbol[1]) + recorrer_preorden(arbol[2]) # Sino se retorna una lista con los elementos iniciando por la raíz luego a los arboles izquierdos listando primero sus padres luego los hijos izquierdos. Una vez recorridos los subarboles izquierdos se recorren los derechos, iniciando por sus padres, luego hijos izquierdos y finalmente los derechos  
 
 def recorrer_inorden(arbol):
     if arbol == []:
-        return []
-    return recorrer_inorden(arbol[1]) + [arbol[0]] + recorrer_inorden(arbol[2])
+        return [] # Si el árbol esta vacío, se retorna una lista vacía.
+    return recorrer_inorden(arbol[1]) + [arbol[0]] + recorrer_inorden(arbol[2]) # Sino se recorre desde los nodos hoja izquierdos hacia sus padres y luego a sus hermanos derechos. Una vez se recorre el el subarbol izquierdo se lista la raiz, luego el subarbol derecho iniciando por las hojas izquierdas hacia sus padres y luego a los hermanos derechos
 
-def recorrer_postorden(arbol):
+def recorrer_postorden(arbol): 
     if arbol == []:
-        return []
-    return recorrer_postorden(arbol[1]) + recorrer_postorden(arbol[2]) + [arbol[0]]
+        return [] # Si el arbol esta vacío, se retorna una lista vacía.
+    return recorrer_postorden(arbol[1]) + recorrer_postorden(arbol[2]) + [arbol[0]] # Sino se recorre el subarbol izquierdo desde los nodos hoja izquierdos hacia sus hermanos derechos y luego al padre de ambos. Una vez recorrido el subarbol izquierdo se salta al nodo hoja izquierdo del subarbol derecho, luego se lista su hermano derecho y finalmente el padre. El proceso se repite hasta finalizar el recorrido del subarbol derecho donde finalmente se visita la raíz del árbol.
 
 # Programa principal
 
